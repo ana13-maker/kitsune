2c2248ae9068ba0dcf1d5e6ce1b10a40ddfd93fb
Safwan Rahman <safwan.rahman15@gmail.com>
2015-11-25 17:50:10 +0600

    smoketest

--- scripts/travis/smoketests.sh
+++ scripts/travis/smoketests.sh
@@ -7,11 +7,15 @@ echo "Starting Browser for Smoke Tests"
 export DISPLAY=:99.0
 
 echo 'Starting a server'
+./manage.py shell < ./scripts/create_user_and_superuser.py
+./manage.py generatedata
 ./manage.py runserver &
 sleep 3
 
 echo 'Running Smoke tests'
 source venv_smoketests/bin/activate
+export VARIABLE_PATH=$('pwd')/scripts/travis/variables.json
+
 cd smoketests
-xvfb-run --server-args="-screen 0, 1280x1024x16" py.test --driver=firefox --baseurl=http://localhost:8000 tests
+xvfb-run --server-args="-screen 0, 1280x1024x16" py.test --driver=firefox --baseurl=http://localhost:8000 --destructive --variables=$VARIABLE_PATH .
 echo 'Booyahkasha!'
