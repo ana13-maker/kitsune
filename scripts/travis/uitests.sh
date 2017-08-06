a35231c469b324f92babb511860645e06993cbad
Safwan Rahman <safwan.rahman15@gmail.com>
2017-08-06 19:47:43 +0600

    fixup

--- scripts/travis/uitests.sh
+++ scripts/travis/uitests.sh
@@ -7,12 +7,12 @@ export DISPLAY=:99.0
 /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
 
 echo 'Starting a server'
-docker-compose exec web ./manage.py create_travis_user_and_superuser.py
+docker-compose exec web ./manage.py create_travis_user_and_superuser
 docker-compose exec web ./manage.py generatedata
 # Reindex elasticsearch
 docker-compose exec web ./manage.py esreindex --delete
 
-GECKO_DRIVER_PATH = ~/geckodriver/geckodriver
+GECKO_DRIVER_PATH="~/geckodriver/geckodriver"
 PYTEST_ADDOPTS="--base-url=http://localhost:8000"
 PYTEST_ADDOPTS="${PYTEST_ADDOPTS} --variables=scripts/travis/variables.json --driver-path=${GECKO_DRIVER_PATH}"
 if [ -n "${MARK_EXPRESSION}" ]; then PYTEST_ADDOPTS="${PYTEST_ADDOPTS} -m=\"${MARK_EXPRESSION}\""; fi
