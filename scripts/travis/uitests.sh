e1c0eb3b0a49452cd75d0d86e8d196a8172e0a95
Safwan Rahman <safwan.rahman15@gmail.com>
2017-08-07 21:52:49 +0600

    update

--- scripts/travis/uitests.sh
+++ scripts/travis/uitests.sh
@@ -12,10 +12,8 @@ docker-compose exec web ./manage.py create_travis_user_and_superuser
 docker-compose exec web ./manage.py generatedata
 # Reindex elasticsearch
 docker-compose exec web ./manage.py esreindex --delete
-
-GECKO_DRIVER_PATH="$HOME/geckodriver/geckodriver"
-PYTEST_ADDOPTS="--verbose --driver=Firefox --base-url=http://localhost:8000"
-PYTEST_ADDOPTS="${PYTEST_ADDOPTS} --variables=scripts/travis/variables.json --driver-path=${GECKO_DRIVER_PATH}"
+PYTEST_BASE_URL="http://localhost:8000"
+PYTEST_ADDOPTS="--verbose --driver=Firefox --variables=scripts/travis/variables.json"
 
 echo 'Running UI tests'
 tox
