43e54bc10ef38040ab107bdc1666f198efeb595b
Safwan Rahman <safwan.rahman15@gmail.com>
2017-08-07 12:22:03 +0600

    last

--- scripts/travis/uitests.sh
+++ scripts/travis/uitests.sh
@@ -4,7 +4,7 @@ set -ex
 
 echo "Starting XVFB for UI tests"
 export DISPLAY=:99.0
-/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
+
 
 echo 'Starting a server'
 docker-compose exec web ./manage.py migrate
