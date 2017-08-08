66761fb360d266f06aa9439bda9380e1685f3fa5
Safwan Rahman <safwan.rahman15@gmail.com>
2017-08-08 10:33:06 +0600

    update

--- scripts/travis/uitests.sh
+++ scripts/travis/uitests.sh
@@ -7,14 +7,13 @@ export DISPLAY=:99.0
 
 
 echo 'Starting a server'
-sh -e /etc/init.d/xvfb start
 docker-compose exec web ./manage.py migrate
 docker-compose exec web ./manage.py create_travis_user_and_superuser
 docker-compose exec web ./manage.py generatedata
 # Reindex elasticsearch
 docker-compose exec web ./manage.py esreindex --delete
-PYTEST_BASE_URL="http://localhost:8000"
-PYTEST_ADDOPTS="--verbose --driver=Firefox --variables=scripts/travis/variables.json"
+export PYTEST_BASE_URL="http://localhost:8000"
+export PYTEST_ADDOPTS="--verbose --driver=Firefox --variables=scripts/travis/variables.json"
 
 echo 'Running UI tests'
 tox
